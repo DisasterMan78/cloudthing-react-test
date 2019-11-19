0572140c1ae097eea8fb7b97a257c54f9666bc25
David Benson <dmbenson1978@gmail.com>
2019-11-19 20:59:43 +0000

    TextInput - conditional styling

--- src/components/TextInput/text-input.tsx
+++ src/components/TextInput/text-input.tsx
@@ -24,33 +24,54 @@ type ErrorMessagesType = {
   [key: string]: string;
 }
 
-const Wrapper = styled.div(`
-        margin: 3rem 2rem 0;
-        text-align: left;
-      `),
-      Label = styled.label(`
-        display: block;
-        color: #fff;
-        font-size: 0.8rem;
-        font-weight: 600;
-        text-transform: uppercase;
-        margin-bottom: 0.5rem;
-      `),
-      Input = styled.input(`
-        width: 100%;
-        height: 4rem;
-        padding-left: 1rem;
-        padding-right: 1rem;
-        color: #000;
-        border: 1px solid #fff;
-        border-radius: 4px;
-        font-size: 1.1rem;
-        letter-spacing: 0.05rem;
-      `),
-      errorMessages: ErrorMessagesType = language.errors,
-      TextInput = ({ name, label, tabIndex, error, register, validation, type }: TextInputProps) => {
+const TextInput = ({ name, label, tabIndex, error, register, validation, type } : TextInputProps) => {
 
-        const renderError = (error: FieldError) => {
+  const errorColour = '#e74843',
+        Wrapper = styled.div`
+          margin: 3rem 2rem 0;
+          text-align: left;
+        `,
+        Label = styled.label`
+          display: block;
+          color: #fff;
+          font-size: 0.8rem;
+          font-weight: 600;
+          text-transform: uppercase;
+          margin-bottom: 0.5rem;
+        `,
+        Input = styled.input`
+          width: 100%;
+          height: 4rem;
+          padding-left: 1rem;
+          padding-right: 1rem;
+          color: #000;
+          border: 1px solid #fff;
+          border-radius: 4px;
+          font-size: 1.1rem;
+          letter-spacing: 0.05rem;
+          &.input-invalid {
+            border-color: ${errorColour};
+            & + .input-error: after {
+              position: absolute;
+              content: "!";
+              right: 1rem;
+              top: -2.5rem;
+              width: 1rem;
+              height: 1rem;
+              color: #fff;
+              background-color: ${errorColour};
+              border-radius: 0.5rem;
+              text-align: center;
+              font-size: 0.7rem;
+              line-height: 1.25rem;
+            }
+          }
+        `,
+        Error = styled.div`
+          position: relative;
+        `,
+        errorMessages: ErrorMessagesType = language.errors,
+        renderError = (error: FieldError) => {
           let key: string = error.type;
 
           if (error.message) {
@@ -61,7 +82,7 @@ const Wrapper = styled.div(`
 
           if (error) {
             return (
-              <div className="input-error">{[message]}</div>
+              <Error className="input-error">{[message]}</Error>
             )
           }
         }
@@ -75,6 +96,7 @@ const Wrapper = styled.div(`
               type={type}
               /* This is nor readable code. See explanation below */
               {...(tabIndex ? { tabIndex } : {})}
+              className={ (error) ? 'input-invalid' : '' }
               ref={register(validation)}
             />
             { renderError(error) }
