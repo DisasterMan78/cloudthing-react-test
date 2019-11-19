79690c59043a4f15088a46ca4e66decafbaaa2fb
David Benson <dmbenson1978@gmail.com>
2019-11-19 11:59:52 +0000

    TextInput: Add conditional tabIndex prop + test (Pass)

--- src/components/TextInput/text-input.test.js
+++ src/components/TextInput/text-input.test.js
@@ -16,6 +16,7 @@ chai.use(sinonChai);
 
 const name = 'input-name',
       label = 'Input Label',
+      tabIndex = '99',
       Component = () => (
         <TextInput
           name={name}
@@ -36,6 +37,24 @@ describe('Song list component', () => {
 
   it('should render a input element with id attribute', () => {
     expect(wrapper.find(`input[id="${name}"]`).length)
+      .to.equal(1);
+  });
+
+  it('should render a the tabindex attribute of the input conditionally', () => {
+    const IndexComponent = () => (
+          // eslint-disable-next-line react/jsx-indent
+            <TextInput
+              name={name}
+              label={label}
+              tabIndex={tabIndex}
+            />
+          ),
+          indexWrapper = mount(<IndexComponent />)
+
+    expect(wrapper.find('input[tabIndex="99"]').length)
+      .to.equal(0);
+
+    expect(indexWrapper.find('input[tabIndex="99"]').length)
       .to.equal(1);
   });
 
--- src/components/TextInput/text-input.tsx
+++ src/components/TextInput/text-input.tsx
@@ -3,16 +3,25 @@ import React from 'react';
 type TextInputProps = {
   name: string;
   label: string;
+  tabIndex: number;
 }
 
-const TextInput = ({ name, label }: TextInputProps) => {
+const TextInput = ({ name, label, tabIndex }: TextInputProps) => {
 
   return (
     <span className="input-wrapper">
       <label htmlFor={name}>{label}</label>
-      <input id={name} type="text" />
+      <input
+        id={name}
+        type="text"
+        {...(tabIndex ? { tabIndex } : {})}
+      />
     </span>
   );
 };
+
+TextInput.defaultProps = {
+  tabIndex: false,
+}
 
 export default TextInput;
