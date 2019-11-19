7adcac3c2905d4bb6439858e4af26f468dcbaa3b
David Benson <dmbenson1978@gmail.com>
2019-11-19 11:08:16 +0000

    TextInput - initial test (fail)

---
+++ src/components/TextInput/text-input.test.js
@@ -0,0 +1,25 @@
+/* globals describe, it */
+import React from 'react';
+
+import chai, { expect } from 'chai';
+import chaiEnzyme from 'chai-enzyme';
+import { mount, configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import sinonChai from 'sinon-chai';
+
+import TextInput from './text-input';
+
+configure({ adapter: new Adapter() }); // configures Enzyme adapter
+
+chai.use(chaiEnzyme());
+chai.use(sinonChai);
+
+const Component = () => (<TextInput />
+      ),
+      wrapper = mount(<Component />);
+
+describe('Song list component', () => {
+  it('should render without crashing', () => {
+    expect(wrapper.exists()).to.equal(true);
+  });
+});
