00ea29522d5095c2370a3e36811640133e8c8448
David Benson <dmbenson1978@gmail.com>
2019-11-19 12:12:40 +0000

    TextInput - Add explanation of conditional prop rendering with ternary operator

--- src/components/TextInput/text-input.tsx
+++ src/components/TextInput/text-input.tsx
@@ -14,11 +14,23 @@ const TextInput = ({ name, label, tabIndex }: TextInputProps) => {
       <input
         id={name}
         type="text"
+        /* This is nor readable code. See explanation below */
         {...(tabIndex ? { tabIndex } : {})}
       />
     </span>
   );
 };
+
+/*
+Re: tabIndex prop
+While concise and clever, that is not very intuitive code.
+It uses a ternary operator [https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Conditional_Operator] and spread to conditionally render the attribute.
+
+It is functionally equivalent to
+  if (tabIndex) {
+    {...tabIndex}
+  }
+*/
 
 TextInput.defaultProps = {
   tabIndex: false,
